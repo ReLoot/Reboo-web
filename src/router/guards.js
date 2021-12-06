22e703acdad2cf703be2419017e55014d6174d8f
liyue <905121829@qq.com>
2021-12-06 16:03:28 +0800

    open sale

--- src/router/guards.js
+++ src/router/guards.js
@@ -22,11 +22,7 @@ const authenticate = (to, from, next, options) => {
 
 const idRefused = (to, from, next, options) => {
   if(to.meta.id_refused==1 && !store.state.common.authentication && store.getters['user/nft']) {
-    console.log(33)
-
     const redirect = commonRedirect()
-    console.log(32)
-
     next(redirect)
   }
   next()
@@ -34,11 +30,7 @@ const idRefused = (to, from, next, options) => {
 
 const idRequired = (to, from, next, options) => {
   if(to.meta.id_required==1 && (!store.state.common.authentication || store.getters['user/nft_benefit']==1)) {
-    console.log(22)
-
     const redirect = commonRedirect()
-    console.log(21)
-
     next(redirect)
   }
   next()
